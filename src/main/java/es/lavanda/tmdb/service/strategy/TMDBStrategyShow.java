4a4922dbeca7da7d54def6c3e45a75a84d936827
Luis Carlos <luisca_jl@hotmail.com>
2022-10-20 15:51:37 +0200

    New regex

--- src/main/java/es/lavanda/tmdb/service/strategy/TMDBStrategyShow.java
+++ src/main/java/es/lavanda/tmdb/service/strategy/TMDBStrategyShow.java
@@ -27,8 +27,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class TMDBStrategyShow implements TMDBStrategy {
 
-    private static final Pattern PATTERN_SHOW_1 = Pattern.compile("(.*).S\\d{1,2}(.*)");
-    private static final Pattern PATTERN_SHOW_2 = Pattern.compile("(.*) S\\d{1,2}(.*)");
+    private static final Pattern PATTERN_SHOW_1 = Pattern.compile("(.*)[. ]S\\d{1,2}(.*)");
     private static final Pattern PATTERN_SHOW_3 = Pattern.compile("(.*).\\d{4}.S\\d{1,2}(.*)");
 
     @Autowired
@@ -99,26 +98,25 @@ public class TMDBStrategyShow implements TMDBStrategy {
         // "/Users/luiscarlos/Documents/Github/LavandaDelPatio/filebot-executor/src/main/resources/filebot/El
         // incidente [BluRay 1080p][DTS 5.1 Castellano DTS-HD 5.1-Ingles+Subs][ES-EN]";
         Matcher matcher1 = PATTERN_SHOW_1.matcher(folderName);
-        Matcher matcher2 = PATTERN_SHOW_2.matcher(folderName);
+        // Matcher matcher2 = PATTERN_SHOW_2.matcher(folderName);
         Matcher matcher3 = PATTERN_SHOW_3.matcher(folderName);
-        if (matcher3.matches()) {
+        if (folderName.contains("[") && folderName.contains("]")) {
+            log.info("Regex  [");
+            return folderName.split("\\[")[0];
+        } else if (folderName.contains("(") && folderName.contains(")")) {
+            log.info("Regex  (");
+            return folderName.split("\\(")[0];
+        } else if (matcher3.matches()) {
             log.info("Regex {}", PATTERN_SHOW_3.pattern());
             return matcher3.group(1).replace(".", " ");
         } else if (matcher1.matches()) {
             log.info("Regex {}", PATTERN_SHOW_1.pattern());
-            // System.out.println("GROUP 1: *" + matcher1.group(1) + "*");
-            // String fileWithDots = generalMatcher.group(1);
             return matcher1.group(1).replace(".", " ");
-        } else if (matcher2.matches()) {
-            log.info("Regex {}", PATTERN_SHOW_2.pattern());
-            // System.out.println("GROUP 1: *" + matcher2.group(1) + "*");
-            // String fileWithDots = matcher2.group(1);
-            return matcher2.group(1).replace(".", " ");
-        } else if (folderName.contains("[") && folderName.contains("]")) {
-            return folderName.split("\\[")[0];
-        } else if (folderName.contains("(") && folderName.contains(")")) {
-            return folderName.split("\\(")[0];
+            // else if (matcher2.matches()) {
+            // log.info("Regex {}", PATTERN_SHOW_2.pattern());
+            // return matcher2.group(1).replace(".", " ");
         } else {
+            log.info("Without regex");
             return folderName;
         }
     }
